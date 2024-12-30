79cc7349e48b7fef42729aac0a1583e72394fa51
NakulPrasad <nakulprasad10@outlook.com>
2024-12-31 02:28:18 +0530

    initial commit

---
+++ src/main/java/com/nakul/foodd_spring/FooddSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nakul.foodd_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakul/foodd_spring/FooddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakul.foodd_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
